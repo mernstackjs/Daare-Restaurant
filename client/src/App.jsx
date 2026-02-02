745e1c71ba16b32f6e58c1de7a4f68ba1f9475e7
Ahmed Abdirizaak <mernstack.js@gmail.com>
2026-02-02 03:00:28 +0100

    add header and menu page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,18 @@
 import { Route, Routes } from "react-router";
+import Header from "./components/Header";
+import Menu from "./pages/menu";
 
 export default function App() {
   return (
     <div>
-      <h1>Welcome to Daare Restaurant</h1>
-      <Routes>
-        <Route path="/" element={<h2>Home Page</h2>} />
-        <Route path="/about" element={<h2>About Us</h2>} />
-        <Route path="/menu" element={<h2>Our Menu</h2>} />
-      </Routes>
+      <Header />
+      <main className="px-12 py-8">
+        <Routes>
+          <Route path="/" element={<h2>Home Page</h2>} />
+          <Route path="/about" element={<h2>About Us</h2>} />
+          <Route path="/menu" element={<Menu />} />
+        </Routes>
+      </main>
     </div>
   );
 }
