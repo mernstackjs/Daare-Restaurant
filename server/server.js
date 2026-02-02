e28408d1647fe535127cc931544103cb64e07c4c
Ahmed Abdirizaak <mernstack.js@gmail.com>
2026-02-02 02:33:24 +0100

    Add server basics and update package

--- server/server.js
+++ server/server.js
@@ -1,1 +1,16 @@
-console.log("Server is starting...");
+import dotenv from "dotenv";
+dotenv.config({ path: "./config/.env" });
+import express from "express";
+import cors from "cors";
+
+const app = express();
+const PORT = process.env.PORT;
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Welcome to Daare Restaurant Server");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
