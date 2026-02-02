ae24d985e9a88c13e6710e65a74ebc4f6c3b4fdb
Ahmed Abdirizaak <mernstack.js@gmail.com>
2026-02-02 02:50:43 +0100

    enhenced cors

--- server/server.js
+++ server/server.js
@@ -5,7 +5,22 @@ import cors from "cors";
 
 const app = express();
 const PORT = process.env.PORT;
-app.use(cors());
+const allowedOrigins = process.env.ALLOW_ORIGIN.split(",");
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  }),
+);
 
 app.get("/", (req, res) => {
   res.send("Welcome to Daare Restaurant Server");
