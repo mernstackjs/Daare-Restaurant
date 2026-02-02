c8f363f3c9f582b3bd1fed34671862357efc91e9
Ahmed Abdirizaak <mernstack.js@gmail.com>
2026-02-02 03:08:34 +0100

    add menu items

--- client/src/pages/menu.jsx
+++ client/src/pages/menu.jsx
@@ -1,16 +1,39 @@
+import { useState } from "react";
 import api from "../api/axios";
 
+import { useEffect } from "react";
+
 export default function Menu() {
-  const getMenu = async () => {
-    const res = await api.get("/api/menu");
-    console.log(res.data);
-  };
+  const [MenuItems, setMenuItems] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const getMenu = async () => {
+      setLoading(true);
+      const res = await api.get("/api/menu");
+      setMenuItems(res.data.menu);
+      setLoading(false);
+    };
+    getMenu();
+  }, []);
+
   return (
     <div>
       <h2>Menu Page</h2>
-      <button className="text-red-600 underline" onClick={getMenu}>
-        Get Menu
-      </button>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <ul className="grid md:grid-cols-3 gap-4">
+          {MenuItems.map((item) => (
+            <li className="border p-4" key={item.id}>
+              {item.name} - ${item.price}
+              <button className="ml-4 px-2 py-1 bg-blue-500 text-white rounded">
+                Order Now
+              </button>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
